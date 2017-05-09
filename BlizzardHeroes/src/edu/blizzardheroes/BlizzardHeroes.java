b70f6b6e14466d33db9975e5678f833b7af3be68
Eugenio Fonseca <eugeniofonseca14@gmail.com>
2017-05-09 17:09:34 -0300

    Interface under development.

--- BlizzardHeroes/src/edu/blizzardheroes/BlizzardHeroes.java
+++ BlizzardHeroes/src/edu/blizzardheroes/BlizzardHeroes.java
@@ -15,8 +15,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.util.Pair;
 
-public class BlizzardHeroes extends Application {
-    
+public class BlizzardHeroes extends Application {    
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
@@ -25,36 +24,6 @@ public class BlizzardHeroes extends Application {
         
         stage.setScene(scene);
         stage.show();
-        
-        int i;
-        int playersNumber = 4;
-        
-        Player[] players = new Player[playersNumber];
-        
-        //The first players always is the Human
-        players[0] = new HumanPlayer("Humano");
-        
-        for(i = 1; i < playersNumber; i++)
-            players[i] = new ComputerPlayer("Computer " + i);
-        
-        Deck deck = new Deck();
-        deck.buildDeck();
-        ArrayList<Card>[] decks = deck.distributeCards(playersNumber);
-        
-        for(i = 0; i < playersNumber; i++)
-            players[i].setCards(decks[i]); 
-        
-        deck.showDecks(players);
-        
-        GameTable table = new GameTable();
-        table.attributeSelected = CardAttribute.DAMAGE;
-        
-        for(i = 0; i < playersNumber; i++)
-            table.currentCards.put(players[i], players[i].getCards().get(0));
-        
-        Pair<Player, Card> winner = table.compareCards();            
-        
-        deck.showDecks(players);
     }   
 
     public static void main(String[] args) {
--- BlizzardHeroes/src/edu/blizzardheroes/MainWindowController.java
+++ BlizzardHeroes/src/edu/blizzardheroes/MainWindowController.java
@@ -1,26 +1,92 @@
 package edu.blizzardheroes;
 
+import edu.blizzardheroes.gameplay.GameTable;
+import edu.blizzardheroes.model.actors.ComputerPlayer;
+import edu.blizzardheroes.model.actors.HumanPlayer;
+import edu.blizzardheroes.model.actors.Player;
+import edu.blizzardheroes.model.cards.Card;
+import edu.blizzardheroes.model.cards.CardAttribute;
+import edu.blizzardheroes.model.cards.Deck;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TitledPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.util.Pair;
 
 public class MainWindowController implements Initializable {
     
-    @FXML
-    private Label label;
+    @FXML GridPane backgroundGrid;
     
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    @FXML TitledPane computer1Frame;
+    @FXML TitledPane computer2Frame;
+    @FXML TitledPane computer3Frame;
+    @FXML TitledPane humanFrame;
     
+    @FXML Label computer1CardCount;
+    @FXML Label computer2CardCount;
+    @FXML Label computer3CardCount;
+    @FXML Label humanCardCount;
+    
+    @FXML ImageView computer1CardImage;
+    @FXML ImageView computer2CardImage;
+    @FXML ImageView computer3CardImage;
+    @FXML ImageView humanCardImage;
+    
+    @FXML Label computer1Stats;
+    @FXML Label computer2Stats;
+    @FXML Label computer3Stats;
+    @FXML Label humanStats;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        backgroundGrid.setStyle(
+                "-fx-background-color: #000000;"
+                + "-fx-background-image: url(\"/edu/blizzardheroes/assets/header_lrg.jpg\");"
+                + "-fx-background-repeat: stretch;"
+                + "-fx-background-position: center center;");
+
+        Image image = new Image("/edu/blizzardheroes/assets/heroes-logo-large.png");
+        computer1CardImage.setImage(image);
+        computer2CardImage.setImage(image);
+        computer3CardImage.setImage(image);
+        humanCardImage.setImage(image);
+        
+        int i;
+        int playersNumber = 4;
+        
+        Player[] players = new Player[playersNumber];
+        
+        //The first players always is the Human
+        players[0] = new HumanPlayer("Humano");
+        
+        for(i = 1; i < playersNumber; i++)
+            players[i] = new ComputerPlayer("Computer " + i);
+        
+        Deck deck = new Deck();
+        deck.buildDeck();
+        ArrayList<Card>[] decks = deck.distributeCards(playersNumber);
+        
+        for(i = 0; i < playersNumber; i++)
+            players[i].setCards(decks[i]); 
+        
+        deck.showDecks(players);
+        
+        GameTable table = new GameTable();
+        table.attributeSelected = CardAttribute.DAMAGE;
+        
+        for(i = 0; i < playersNumber; i++)
+            table.currentCards.put(players[i], players[i].getCards().get(0));
+        
+        Pair<Player, Card> winner = table.compareCards();            
+        
+        deck.showDecks(players);
     }    
     
 }
